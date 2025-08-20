43ca75cb988db7a9c3d9161525731c204c8bd639
TKU-FCP-411631137 <mfc930223@gmail.com>
2025-08-20 11:27:31 +0800

    Update M08_BSTRangedSum.java

--- midterm/M08_BSTRangedSum.java
+++ midterm/M08_BSTRangedSum.java
@@ -24,6 +24,7 @@ public class M08_BSTRangedSum {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
         int L = Integer.parseInt(st.nextToken());
